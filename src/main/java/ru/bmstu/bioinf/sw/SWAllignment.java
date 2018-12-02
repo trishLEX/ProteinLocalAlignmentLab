fd2a7cbc473b11904ec7c5222740c44083280606
sadpug <serterman@gmail.com>
2018-12-02 21:41:36 +0300

    merged \w master Boxed типы заменены на примитивные.

--- src/main/java/ru/bmstu/bioinf/sw/SWAllignment.java
+++ src/main/java/ru/bmstu/bioinf/sw/SWAllignment.java
@@ -2,7 +2,7 @@ package ru.bmstu.bioinf.sw;
 
 import javafx.util.Pair;
 import ru.bmstu.bioinf.FineTable;
-import ru.bmstu.bioinf.Node;
+import ru.bmstu.bioinf.filtering.Node;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -12,13 +12,9 @@ public class SWAllignment {
     private String fromBase;
     private Map<Pair<Integer, Integer>, SWNode> matrix;
     private FineTable table;
-    private Integer top;
-    private Integer bottom;
-    private Integer left;
-    private Integer right;
     private SWNode maxNode;
 
-    private static final Integer borderAdd = 32;
+    private static final int borderAdd = 32;
 
 
     public SWAllignment(String searched, String fromBase,
@@ -28,38 +24,38 @@ public class SWAllignment {
         this.table = table;
 
         Pair<Integer, Integer> borders = getBorder(routeStart, routeFinish);
-        Integer top = borders.getKey();
-        Integer bottom = borders.getValue();
-        Integer left = routeStart.getDataSetSeqCoordinate() + routeStart.getSearchedSeqCoordinate();
-        Integer right = routeFinish.getDataSetSeqCoordinate() + routeFinish.getSearchedSeqCoordinate();
+        int top = borders.getKey();
+        int bottom = borders.getValue();
+        int left = routeStart.getDataSetSeqCoordinate() + routeStart.getSearchedSeqCoordinate();
+        int right = routeFinish.getDataSetSeqCoordinate() + routeFinish.getSearchedSeqCoordinate();
 
         computeMatrix(top, bottom, left, right);
     }
 
-    private void computeMatrix(Integer top, Integer bottom, Integer left, Integer right) {
+    private void computeMatrix(int top, int bottom, int left, int right) {
         matrix = new LinkedHashMap<>();
         maxNode = new SWNode(0,0, 0.0f, SWNode.getZeroNode());
 
-        Integer searchStartCoord = Integer.max((left - top) / 2, 0);
-        Integer searchUntilCoord = Integer.min((right - bottom) / 2 + 1, searched.length());
+        int searchStartCoord = Integer.max((left - top) / 2, 0);
+        int searchUntilCoord = Integer.min((right - bottom) / 2 + 1, searched.length());
 
-        for(Integer i = searchStartCoord; i < searchUntilCoord; ++i) {
-            Integer start = Integer.max(
+        for(int i = searchStartCoord; i < searchUntilCoord; ++i) {
+            int start = Integer.max(
                     Integer.max(left - i, bottom + i),
                     0);
-            Integer until = Integer.min(
+            int until = Integer.min(
                     Integer.min(right - i + 1, top + i + 1),
                     fromBase.length());
             for(Integer j = start; j < until; ++j) {
                 SWNode nodeij = SWNode.max(
                         SWNode.getZeroNode(),
-                        new SWNode(i, j, (float) table.getE(),
+                        new SWNode(i, j, table.getE(),
                                 matrix.getOrDefault(
                                         new Pair<>(i - 1, j),
                                         new SWNode(
                                                 i - 1, j, (float) 0, SWNode.getZeroNode()))
                         ),
-                        new SWNode(i, j, (float) table.getE(),
+                        new SWNode(i, j, table.getE(),
                                 matrix.getOrDefault(
                                         new Pair<>(i, j - 1),
                                         new SWNode(
@@ -81,12 +77,12 @@ public class SWAllignment {
     //Возвращает пару (верхняя диагональ, нижняя диагональ) + дополнительный отступ (где возможно)
     private Pair<Integer, Integer> getBorder(Node routeStart, Node routeFinish) {
         Node curNode = routeFinish;
-        Integer topDiag =
+        int topDiag =
                 routeFinish.getDataSetSeqCoordinate() - routeFinish.getSearchedSeqCoordinate();
-        Integer bottomDiag = topDiag;
+        int bottomDiag = topDiag;
         while(curNode != routeStart) {
             curNode = curNode.getMaxParent();
-            Integer candidate =
+            int candidate =
                     curNode.getDataSetSeqCoordinate() - curNode.getSearchedSeqCoordinate();
             topDiag = Integer.max(topDiag, candidate);
             bottomDiag = Integer.min(bottomDiag, candidate);
@@ -162,21 +158,21 @@ public class SWAllignment {
 }
 
 class SWNode {
-    public Integer getSearchedCoord() {
+    public int getSearchedCoord() {
         return searchedCoord;
     }
 
-    public Integer getBaseCoord() {
+    public int getBaseCoord() {
         return baseCoord;
     }
 
-    private Integer searchedCoord;
-    private Integer baseCoord;
+    private int searchedCoord;
+    private int baseCoord;
     private SWNode parent;
     private Float score;
     private static SWNode zero = null;
 
-    public SWNode(Integer searchedCoord, Integer baseCoord, Float addition, SWNode parent) {
+    public SWNode(int searchedCoord, int baseCoord, float addition, SWNode parent) {
         this.searchedCoord = searchedCoord;
         this.baseCoord = baseCoord;
         this.parent = parent;
@@ -191,7 +187,7 @@ class SWNode {
         return parent;
     }
 
-    public Float getScore() {
+    public float getScore() {
         return score;
     }
 
