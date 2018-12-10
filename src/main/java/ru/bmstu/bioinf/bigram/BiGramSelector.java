0e1eb291bd18f16f97291e3dae605e2fe60cbecc
trishLEX <alexeytr5@gmail.com>
2018-12-10 17:31:02 +0300

    Удален лишний код

--- src/main/java/ru/bmstu/bioinf/bigram/BiGramSelector.java
+++ src/main/java/ru/bmstu/bioinf/bigram/BiGramSelector.java
@@ -11,30 +11,10 @@ import java.util.*;
 public class BiGramSelector {
     private Sequence searchedSequence;
     private Sequence dataSetSequence;
-    private BiGramIterator searchedIter;
-    private BiGramIterator dataSetIter;
 
     public BiGramSelector(Sequence searchedSequence, Sequence dataSetSequence) {
         this.searchedSequence = searchedSequence;
         this.dataSetSequence = dataSetSequence;
-        this.searchedIter = new BiGramIterator(searchedSequence);
-        this.dataSetIter = new BiGramIterator(this.dataSetSequence);
-    }
-
-    /**
-     * Выделяет набор bigram, используя итераторы
-     * @return набор bigram для заданной searchedSequence
-     */
-    public List<BiGram> getNewNGramsByIterator() {
-         List<BiGram> biGrams = new ArrayList<>();
-
-         while (searchedIter.hasNext()) {
-             BiGramIterator.NGramStruct current = searchedIter.next();
-             List<BiGram> result = getNewNGram(current);
-             biGrams.addAll(result);
-         }
-
-         return biGrams;
     }
 
     public List<Set<Node>> getNewNGramsByHash() {
@@ -74,100 +54,5 @@ public class BiGramSelector {
         }
 
         return diags;
-    }
-
-    /**
-     * Сопоставляет n-грамму из searched и n-граммы из data
-     * В случае успеха, возвращает список объектов класса BiGram
-     * Если searched или data последовательность закончена, возвращает пустой список
-     * @return список bigram или пустой список
-     */
-    private List<BiGram> getNewNGram(BiGramIterator.NGramStruct searchedNGramSeq) {
-        dataSetIter.setPos(0); // Для новой ngramm начинаем поиск с нуля
-        List<BiGram> result = new ArrayList<>();
-
-        while (dataSetIter.hasNext()) {
-            BiGramIterator.NGramStruct dataNgramSeq = dataSetIter.next();
-
-            if (searchedNGramSeq.getnGram().equals(dataNgramSeq.getnGram())) {
-                result.add(new BiGram(searchedNGramSeq.getnGram(), searchedNGramSeq.getPos(), dataNgramSeq.getPos()));
-            }
-        }
-
-        return result;
-    }
-
-    /**
-     * Выделяет набор {@link BiGram} внутри dataSetSequence с помощью алгоритма КМП
-     * @return набор {@link BiGram} для заданной searchedSequence
-     */
-    public List<BiGram> getNewNGrams() {
-        List<BiGram> ngrams = new ArrayList<>();
-
-        while (searchedIter.hasNext()) {
-            BiGramIterator.NGramStruct current = searchedIter.next();
-            List<Integer> indices = KMPSubstr(current.getnGram());
-            if (indices == null) {
-                continue;
-            }
-
-            for (Integer index : indices) {
-                ngrams.add(new BiGram(current.getnGram(), current.getPos(), index));
-            }
-        }
-
-        return ngrams;
-    }
-
-    /**
-     * Вспомогательный алгоритм для КМП
-     * Выделяет префикс
-     */
-    private int[] prefix(String s) {
-        int n = s.length();
-        int[] pi = new int[n];
-        Arrays.fill(pi, 0);
-
-        int j;
-        for (int i = 1; i < n; i++) {
-            j = pi[i - 1];
-            while(j > 0 && s.charAt(i) != s.charAt(j)) {
-                j = pi[j - 1];
-            }
-            if (s.charAt(i) == s.charAt(j)) {
-                j++;
-            }
-            pi[i] = j;
-        }
-
-        return pi;
-    }
-
-    /**
-     * Алгоритм Кнута-Морриса-Пратта
-     * @param searchedNGram последовательность, вхождение которой нужно найти
-     * @return набор координат из dataSet, в которых входная последовательность существует
-     */
-    private List<Integer> KMPSubstr(String searchedNGram) {
-        List<Integer> entries = new ArrayList<>();
-
-        int[] pi = prefix(searchedNGram);
-
-        int q = 0;
-
-        for(int k = 0; k < dataSetSequence.length(); k++) {
-            while (q > 0 && searchedNGram.charAt(q) != dataSetSequence.get(k)) {
-                q = pi[q - 1];
-            }
-            if (searchedNGram.charAt(q) == dataSetSequence.get(k)) {
-                q++;
-            }
-            if (q == searchedNGram.length()) {
-                entries.add(k - searchedNGram.length() + 1);
-                q = 0;
-            }
-        }
-
-        return entries;
     }
 }
--- src/test/java/ru/bmstu/bioinf/bigram/BiGramSelectorTest.java
+++ src/test/java/ru/bmstu/bioinf/bigram/BiGramSelectorTest.java
@@ -9,86 +9,8 @@ import java.util.List;
 import java.util.Set;
 
 public class BiGramSelectorTest {
-    /**
-     * Тесты для метода КМП
-     **/
     @Test
     public void test1() {
-        Sequence searchedSeq = new Sequence("searched", "AGACGTAAGA");
-        Sequence dataSeq = new Sequence("data", "AGGGGGGGGGGGGGGGGGGG");
-        BiGramSelector biGramSelector = new BiGramSelector(searchedSeq, dataSeq);
-        List<BiGram> result = biGramSelector.getNewNGrams();
-        Assert.assertEquals(2, result.size());
-        Assert.assertEquals(0, result.get(0).getSearchedSeqCoordinate());
-        Assert.assertEquals(0, result.get(0).getDataSetSeqCoordinate());
-        Assert.assertEquals(7, result.get(1).getSearchedSeqCoordinate());
-        Assert.assertEquals(0, result.get(0).getDataSetSeqCoordinate());
-    }
-
-    @Test
-    public void test2() {
-        Sequence searchedSeq = new Sequence("searched", "");
-        Sequence dataSeq = new Sequence("data", "");
-        BiGramSelector biGramSelector = new BiGramSelector(searchedSeq, dataSeq);
-        List<BiGram> result = biGramSelector.getNewNGrams();
-        Assert.assertEquals(0, result.size());
-    }
-
-    @Test
-    public void test3() {
-        Sequence searchedSeq = new Sequence("searched", "AGAG");
-        Sequence dataSeq = new Sequence("data", "AGAG");
-        BiGramSelector biGramSelector = new BiGramSelector(searchedSeq, dataSeq);
-        List<BiGram> result = biGramSelector.getNewNGrams();
-        Assert.assertEquals(5, result.size());
-    }
-
-    @Test
-    public void test4() {
-        Sequence searchedSeq = new Sequence("searched", "AGAG");
-        Sequence dataSeq = new Sequence("data", "AGAG");
-        BiGramSelector biGramSelector = new BiGramSelector(searchedSeq, dataSeq);
-        List<BiGram> result = biGramSelector.getNewNGrams();
-        Assert.assertEquals(5, result.size());
-        Assert.assertEquals("AG", result.get(1).getString());
-    }
-
-    /**
-     * Те же самые тесты, но для метода итератором
-     **/
-    @Test
-    public void test6() {
-        Sequence searchedSeq = new Sequence("searched", "AGACGTAAGA");
-        Sequence dataSeq = new Sequence("data", "AGGGGGGGGGGGGGGGGGGG");
-        BiGramSelector biGramSelector = new BiGramSelector(searchedSeq, dataSeq);
-        List<BiGram> result = biGramSelector.getNewNGramsByIterator();
-        Assert.assertEquals(2, result.size());
-        Assert.assertEquals(0, result.get(0).getSearchedSeqCoordinate());
-        Assert.assertEquals(0, result.get(0).getDataSetSeqCoordinate());
-        Assert.assertEquals(7, result.get(1).getSearchedSeqCoordinate());
-        Assert.assertEquals(0, result.get(0).getDataSetSeqCoordinate());
-    }
-
-    @Test
-    public void test7() {
-        Sequence searchedSeq = new Sequence("searched", "");
-        Sequence dataSeq = new Sequence("data", "");
-        BiGramSelector biGramSelector = new BiGramSelector(searchedSeq, dataSeq);
-        List<BiGram> result = biGramSelector.getNewNGramsByIterator();
-        Assert.assertEquals(0, result.size());
-    }
-
-    @Test
-    public void test8() {
-        Sequence searchedSeq = new Sequence("searched", "AGAG");
-        Sequence dataSeq = new Sequence("data", "AGAG");
-        BiGramSelector biGramSelector = new BiGramSelector(searchedSeq, dataSeq);
-        List<BiGram> result = biGramSelector.getNewNGramsByIterator();
-        Assert.assertEquals(5, result.size());
-    }
-
-    @Test
-    public void test11() {
         Sequence searchedSeq = new Sequence("searched", "AGACGTAAGA");
         Sequence dataSeq = new Sequence("data", "AGGGGGGGGGGGGGGGGGGG");
         BiGramSelector biGramSelector = new BiGramSelector(searchedSeq, dataSeq);
@@ -97,7 +19,7 @@ public class BiGramSelectorTest {
     }
 
     @Test
-    public void test12() {
+    public void test2() {
         Sequence searchedSeq = new Sequence("searched", "AABA");
         Sequence dataSeq = new Sequence("data", "ABAA");
         BiGramSelector biGramSelector = new BiGramSelector(searchedSeq, dataSeq);
